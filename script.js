57cec9ba138ffb13c79657a7ab7deabb2087b2b8
screact02 <screact02@gmail.com>
2023-04-06 19:43:40 -0400

    Added a functionality to strike a task

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 // console.log("linked");
 
-const userinput = document.getElementById("user-input");
-function addTodo(){
-const createElement = document.createElement("input");
-createElement.setAttribute("type","checkbox");
-document.getElementById("checkbox").appendChild(createElement);
 
-const todolist = document.getElementById("list");
-todolist.innerText = userinput.value;
+function addTodo(){
+  const userinput = document.getElementById("user-input");
+const createContainer = document.createElement("div");
+createContainer.id= "task";
+createContainer.style = "display:flex";
+createContainer.style = "background-color:gold";
+document.getElementById("content").appendChild(createContainer);
+const createCheckbox = document.createElement("input");
+createCheckbox.setAttribute("type","checkbox");
+document.getElementById("task").appendChild(createCheckbox);
+
+const createParagraph = document.createElement("label");
+document.getElementById("task").appendChild(createParagraph);
+createParagraph.style = "margin:0";
+createParagraph.innerText = userinput.value;
+
+createCheckbox.onclick = function(){
+  createParagraph.style = "text-decoration-line: line-through";
+  const createRemoveButton = document.createElement("button");
+  createRemoveButton.id = "removeTask";
+  createRemoveButton.setAttribute('value', 'remove task');
+  document.getElementById("task").appendChild(createRemoveButton);
+  createRemoveButton.onclick = function(){
+    createContainer.style = "display:none";
+
+  }
+}
 }
 
